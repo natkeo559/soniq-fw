3b207d9411ca2e9c3ad9703da7035f17bf0ba294
natkeo559 <nhkeoug@gmail.com>
2025-03-30 20:15:06 -0400

    Added SD Card Configuration

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,20 @@ use core::mem;
 use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
-use embassy_rp::config::Config;
+use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::PIO0;
 use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::spi::{Config, Spi};
 use embassy_sync::channel::{Receiver, Sender};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
+use embassy_time::Delay;
+use embedded_hal_bus::spi::ExclusiveDevice;
 use mic::{PioI2sMic, PioI2sMicProgram};
 use panic_probe as _;
+use sd::DummyTime;
 
 mod mic;
+mod sd;
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
@@ -23,21 +28,42 @@ bind_interrupts!(struct Irqs {
 const SAMPLE_RATE: u32 = 48_000;
 const BUFFER_SIZE: usize = 960;
 const CHANNEL_CAP: usize = 1;
+
 static mut DMA_BUFFER: [u32; BUFFER_SIZE * 2] = [0; BUFFER_SIZE * 2];
+
 static BUFFER_CHANNEL: Channel<CriticalSectionRawMutex, [u32; BUFFER_SIZE], CHANNEL_CAP> =
     Channel::new();
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    let p = embassy_rp::init(Config::default());
+    let p = embassy_rp::init(embassy_rp::config::Config::default());
 
-    let Pio {
-        mut common, sm0, ..
-    } = Pio::new(p.PIO0, Irqs);
-
+    // MIC PINS
     let bit_clock_pin = p.PIN_18;
     let left_right_clock_pin = p.PIN_19;
     let data_pin = p.PIN_20;
+
+    // SD CARD PINS
+    let sck = p.PIN_10;
+    let mosi = p.PIN_11;
+    let miso = p.PIN_12;
+    let cs_pin = Output::new(p.PIN_13, Level::High);
+
+    // Configure SPI for SD card init.
+    let mut spi_config = Config::default();
+    spi_config.frequency = 400_000; // 400 kHz
+    spi_config.phase = embassy_rp::spi::Phase::CaptureOnFirstTransition;
+    spi_config.polarity = embassy_rp::spi::Polarity::IdleLow;
+
+    let spi_bus = Spi::new_blocking(p.SPI1, sck, mosi, miso, spi_config);
+    let spi_dev = ExclusiveDevice::new(spi_bus, cs_pin, Delay).unwrap();
+
+    sd::try_sd_init(spi_dev, Delay, DummyTime).unwrap();
+
+    // Configure MIC PIO state machine and driver.
+    let Pio {
+        mut common, sm0, ..
+    } = Pio::new(p.PIO0, Irqs);
 
     let program: PioI2sMicProgram<'_, PIO0> = PioI2sMicProgram::new(&mut common);
 
@@ -81,6 +107,7 @@ async fn sd_task(
     loop {
         let recv_buf: [u32; BUFFER_SIZE] = receiver.receive().await;
 
+        // TODO: Update WAV file on the mounted volume (sd card) via `embedded_sdmmc`
         defmt::info!("First sample in back buffer: {=u32}", recv_buf[0]);
     }
 }
---
+++ src/sd.rs
@@ -0,0 +1,72 @@
+use embedded_sdmmc::{
+    Error, Mode, SdCard, SdCardError, TimeSource, Timestamp, VolumeIdx, VolumeManager,
+};
+
+#[allow(unused)]
+pub fn create_wav_header(
+    sample_rate: u32,
+    channels: u16,
+    bits_per_sample: u16,
+    data_len: u32,
+) -> [u8; 44] {
+    let byte_rate = sample_rate * u32::from(channels) * u32::from(bits_per_sample) / 8;
+    let block_align = channels * (bits_per_sample / 8);
+    let chunk_size = 36 + data_len;
+    let mut header = [0u8; 44];
+
+    header[0..4].copy_from_slice(b"RIFF");
+    header[4..8].copy_from_slice(&chunk_size.to_le_bytes());
+    header[8..12].copy_from_slice(b"WAVE");
+
+    header[12..16].copy_from_slice(b"fmt ");
+    header[16..20].copy_from_slice(&16u32.to_le_bytes()); // PCM header size
+    header[20..22].copy_from_slice(&1u16.to_le_bytes()); // PCM format
+    header[22..24].copy_from_slice(&channels.to_le_bytes());
+    header[24..28].copy_from_slice(&sample_rate.to_le_bytes());
+    header[28..32].copy_from_slice(&byte_rate.to_le_bytes());
+    header[32..34].copy_from_slice(&block_align.to_le_bytes());
+    header[34..36].copy_from_slice(&bits_per_sample.to_le_bytes());
+
+    header[36..40].copy_from_slice(b"data");
+    header[40..44].copy_from_slice(&data_len.to_le_bytes());
+
+    header
+}
+
+// Dummy `TimeSource` for `embedded_sdmmc`
+pub struct DummyTime;
+
+impl TimeSource for DummyTime {
+    fn get_timestamp(&self) -> Timestamp {
+        Timestamp {
+            year_since_1970: 55, // e.g. 2025
+            zero_indexed_month: 0,
+            zero_indexed_day: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+        }
+    }
+}
+
+pub fn try_sd_init<S, D, T>(spi: S, delay: D, ts: T) -> Result<(), Error<SdCardError>>
+where
+    S: embedded_hal::spi::SpiDevice,
+    D: embedded_hal::delay::DelayNs,
+    T: TimeSource,
+{
+    let sdcard = SdCard::new(spi, delay);
+    defmt::info!("Card size is {} bytes", sdcard.num_bytes()?);
+    let mut volume_mgr = VolumeManager::new(sdcard, ts);
+    let mut volume0 = volume_mgr.open_volume(VolumeIdx(0))?;
+    let mut root_dir = volume0.open_root_dir()?;
+    let mut my_file = root_dir.open_file_in_dir("hello.txt", Mode::ReadOnly)?;
+    while !my_file.is_eof() {
+        let mut buffer = [0u8; 32];
+        let num_read = my_file.read(&mut buffer)?;
+        unsafe {
+            defmt::info!("{}", core::str::from_utf8_unchecked(&buffer[0..num_read]));
+        }
+    }
+    Ok(())
+}
